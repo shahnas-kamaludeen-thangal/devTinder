9d8a393dbb08fec8ff2cdafa6b687d8c66ffeba7
shahnas <shahnas.kamaludeen@gmail.com>
2025-12-11 15:22:07 -0600

    added routes

--- src/index.js
+++ src/index.js
@@ -13,4 +13,12 @@ app.get("/about", (req, res) => {
 app.get("/Home", (req, res) => {
   res.send("I am from Home");
 });
+app.post("/user", (req, res) => {
+  const { name, place } = req.query;
+  res.send({ name, place });
+});
+app.post("/user/:id/:name/:place", (req, res) => {
+  const { id, name, place } = req.params;
+  res.send({ id, name, place });
+});
 app.listen(3000, () => console.log("Server is listening to port 3000"));
